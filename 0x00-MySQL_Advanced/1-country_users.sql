d34825193d55bf646e6db6b90ecaa652cff64a42
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-23 18:46:45 +0100

    Add comma

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- Creates a users table with
--- id, email and name fields
+-- id, email, name, country(enumeration of countries: US, CO and TN) fields
 CREATE TABLE if NOT EXISTS users (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 )
