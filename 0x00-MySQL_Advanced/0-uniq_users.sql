a5c9df0a461fc64ea6e382fc8d2762db5b3d5b8b
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-23 18:29:16 +0100

    Create 0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Creates a users table with
+-- id, email and name fields
+CREATE TABLE if NOT EXISTS users (
+    id INT AUTO_INCREAMENT NOT NULL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+)
