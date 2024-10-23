90b091fb69e1464666fcc54152f3d88a399f22e9
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-23 18:42:37 +0100

    Create 1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates a users table with
+-- id, email and name fields
+CREATE TABLE if NOT EXISTS users (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+)
