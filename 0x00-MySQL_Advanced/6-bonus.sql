65cf10ee65375d8e5369945e9539cf598b1c01d3
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-24 17:28:23 +0100

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,12 +1,13 @@
 DELIMITER //
 
-CREATE PROCEDURE AddBonus (
+CREATE PROCEDURE AddBonus(
     IN user_id INT,
     IN project_name VARCHAR(255),
     IN score INT
 )
 BEGIN
     DECLARE project_id INT;
+
     SELECT id INTO project_id
     FROM projects
     WHERE name = project_name;
@@ -16,7 +17,8 @@ BEGIN
         SET project_id = LAST_INSERT_ID();
     END IF;
 
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, project_id, score);
 END;
 //
 
