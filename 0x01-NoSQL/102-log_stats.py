a5a02aaa4f42a8a7be62ba8e0413950fecde87f8
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-30 19:19:00 +0100

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -4,6 +4,7 @@ Provide some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
+
 def log_stats():
     """
     Database: logs, Collection: nginx,
@@ -14,7 +15,8 @@ def log_stats():
     collection = db.nginx
 
     total_logs = collection.count_documents({})
-    print(f"{total_logs: logs}")
+    print(f"{total_logs} logs")
+
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
