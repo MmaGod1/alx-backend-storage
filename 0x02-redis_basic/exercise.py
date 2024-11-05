099f8c71c6d299dd0634b3efc016ac6165408f77
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-05 18:23:34 +0100

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module to define redis Python classes"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -16,3 +16,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+   def get(self, key: str,
+           fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Take a key string argument and an optional Callable argument named fn.
+        This callable will be used to convert the data back to the desired format."""
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+  def get_str(self, key: str) -> str:
+      """Automatically parametrize Cache.get to a str."""
+      return self._redis.get(key).decode("utf-8")
+
+  def get_int(self, key: int) -> int:
+      """Automatically parametrize Cache.get to an int"""
+      return self.get(key, fn=int)
